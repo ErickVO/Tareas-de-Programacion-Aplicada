90191f3b7039c80916747e7ebd064e6a27f25f9e
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-01-18 17:58:11 -0400

    Add files via upload

---
+++ Ejercicio4_1/Ejercicio4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string num;
+
+            Console.WriteLine("Introduzca un Numero: ");
+            num = Console.ReadLine();
+            n = Convert.ToInt32(num);
+
+            Console.Clear();
+            Console.WriteLine("Tabla de Multiplicacion del {0}", n);
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(n + "x" + i + "= " + (n * i));
+            }
+
+        }
+    }
+}
---
+++ Ejercicio4_2/Ejercicio4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bas,pot, i = 0,r = 1;
+            string b,p;
+
+            Console.WriteLine("Introduzca una base");
+            b = Console.ReadLine();
+            bas = Convert.ToInt32(b);
+            Console.WriteLine("Introduzca una potencia");
+            p = Console.ReadLine();
+            pot = Convert.ToInt32(p);
+
+            do
+            {
+                r = bas * r;
+                
+               i++;                
+
+            } while (i != pot);
+            Console.WriteLine("El Resultado es: {0}", r);
+        }
+    }
+}
