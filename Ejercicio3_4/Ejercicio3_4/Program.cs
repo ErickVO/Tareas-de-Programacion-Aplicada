1fb2ad6b1aba36088f344cfe3a8989f1e7ca43e6
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-01-18 17:56:14 -0400

    Add files via upload

---
+++ Ejercicio3_4/Ejercicio3_4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ejercicio3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            string n;
+
+            Console.WriteLine("Introduzca un Numero del 1 al 7: ");
+            n = Console.ReadLine();
+
+            num = Convert.ToInt32(n);
+
+            if(num == 1)
+            {
+                Console.WriteLine("Lunes");
+
+            }else if (num == 2)
+            {
+                Console.WriteLine("Martes");
+            }
+            else if (num == 3)
+            {
+                Console.WriteLine("Miercoles");
+            }
+            else if (num == 4)
+            {
+                Console.WriteLine("Jueves");
+            }
+            else if(num == 5)
+            {
+                Console.WriteLine("Viernes");
+            }
+            else if (num == 6)
+            {
+                Console.WriteLine("Sabado");
+            }
+            else if (num == 7)
+            {
+                Console.WriteLine("Domingo");
+            }
+            else
+                Console.WriteLine("Numero no existente");
+        }
+    }
+}
---
+++ Ejercicio3_5/Ejercicio3_5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Ejercicio3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0,apo = 0,r = 0, selec;
+            string LLados, Nlados, Apotema, seleccion;
+            float area;
+
+            Console.WriteLine("Seleccione lo que desea buscar: " + "\n1. Perimetro" +"\n2. Area");
+            seleccion = Console.ReadLine();
+            selec = Convert.ToInt32(seleccion);
+
+            Console.Clear();
+
+            Console.WriteLine("Introduzca la Cantidad de lados:");
+            Nlados = Console.ReadLine();
+            Console.WriteLine("Introduzca la Longitud de un Lado:");
+            LLados = Console.ReadLine();
+
+            a = Convert.ToInt32(LLados);
+            b = Convert.ToInt32(Nlados);
+
+            if (selec == 1)
+            {
+                
+
+                r = a * b;
+
+                Console.WriteLine("El Perimetro es: {0}", r);
+            }
+            else if (selec == 2)
+            {
+                Console.WriteLine("Introduzca el Apotema:");
+                Apotema = Console.ReadLine();
+                apo = Convert.ToInt32(Apotema);
+
+                area = (a * b * apo) / 2;
+
+                Console.WriteLine("El Area es: {0}", area);
+            }
+            
+
+        }
+    }
+}
