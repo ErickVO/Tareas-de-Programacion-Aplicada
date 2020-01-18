1733c2095424fe43cf628d45eddcfeef43229da3
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-01-18 18:01:32 -0400

    Add files via upload

---
+++ EjercicioPerimetroPoligono/EjercicioPerimetroPoligono/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EjercicioPerimetroPoligono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, r = 0;
+            string LLados, Nlados;
+
+            Console.WriteLine("Introduzca la Cantidad de lados:");
+            Nlados = Console.ReadLine();
+            Console.WriteLine("Introduzca la Longitud de un Lado:");
+            LLados = Console.ReadLine();
+
+
+            a = Convert.ToInt32(LLados);
+            b = Convert.ToInt32(Nlados);
+
+            r = a * b;
+
+            Console.WriteLine("El Perimetro es: {0}",r);
+            
+        }
+    }
+}
---
+++ GradosRadianes/GradosRadianes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GradosRadianes
+{
+    class Program
+    {
+        private const int radianes = 180;
+        static void Main(string[] args)
+        {
+            float grados = 0, resultado = 0;
+            string a;
+
+            Console.WriteLine("Introduzca los Grados: ");
+            a = Console.ReadLine();
+
+            grados = Convert.ToSingle(a);
+
+            resultado = grados / radianes;
+
+            Console.Write("Los Radianes son: {0}", resultado + " Radianes");
+
+        }
+    }
+}
