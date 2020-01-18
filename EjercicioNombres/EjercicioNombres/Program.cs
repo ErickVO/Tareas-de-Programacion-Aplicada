1e6b98af5e0c5bff2e4fec31c685fdfdc1806046
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-01-18 18:00:02 -0400

    Add files via upload

---
+++ EjercicioNombres/EjercicioNombres/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EjercicioNombres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Erick Vicente");
+            Console.Write("Objects of the Class: ");
+            Console.Write("Door");
+            Console.Write(", Window");
+        }
+    }
+}
