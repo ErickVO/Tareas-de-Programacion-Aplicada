78f2ba8f28606eb185b11fc79baa26dff8ff5eaa
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-01-25 13:21:32 -0400

    Add files via upload

---
+++ Tarea2/Ejercicio_Fecha/Ejercicio7_3_ACliente/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Ejercicio7_3_ACliente
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Hashtable Diccionario = new Hashtable();
+            Diccionario.Add("Badulaque", "Persona necia, inconsistente.");
+            Diccionario.Add("Apodyopsis", "El acto de desverstir mentalmente a una persona.");
+            Diccionario.Add("Kalopsia", "La ilusion de creer que algo es mas lindo de lo que en realidad es.");
+
+            Console.WriteLine("|-|-| Diccionario |-|-| ");
+            foreach (DictionaryEntry datos in Diccionario)
+            Console.WriteLine("\n–{0}:   {1}", datos.Key, datos.Value);
+
+        }
+
+
+
+    }
+}
---
+++ Tarea2/Ejercicio_Fecha/Factorial_de_un_numero/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Factorial_de_un_numero
+{
+    class Program
+    {
+        //Ejercicio_5_4
+     
+        static void Main(string[] args)
+        {
+            String numero;
+            int num,resultado = 0;
+
+            Console.WriteLine("Introduzca el Numero: ");
+            numero = Console.ReadLine();
+
+            num = Convert.ToInt32(numero);
+
+            resultado = Factorial(num);
+
+            Console.WriteLine("Resultado: {0}", resultado);
+        }
+
+        public static int Factorial(int a)
+        {
+            int numi = 1, numf = 2, i = 1;
+
+            do
+            {
+
+                numi = numi * numf;
+                numf = numf + 1;
+                i++;
+    
+
+        } while (i < a);
+
+            return  numi;
+
+        }
+
+
+
+
+
+    }
+
+    
+
+}
