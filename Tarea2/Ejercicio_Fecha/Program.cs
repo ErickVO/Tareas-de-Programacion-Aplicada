25ac72c96c5965200f6d2f7d6e332e6defabca95
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-01-25 13:16:28 -0400

    Add files via upload

---
+++ Tarea2/Ejercicio_Fecha/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ejercicio_Fecha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String formato;
+
+            formato = String.Format("La fecha es: {0:HH:mmtt yyyy dd MMM }", DateTime.Now);
+            Console.WriteLine(formato);
+        }
+    }
+}
